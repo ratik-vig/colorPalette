79494b0f6f22a5fcead66b8de04575d0fba28312
Ratik Vig <ratikvig@gmail.com>
2020-09-19 19:43:01 +0530

    added color overlay

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,19 +1,37 @@
 import React, {Component} from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 class ColorBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            copied: false
+        }
+    }
+
+    handleCopy = () => {
+        this.setState({copied: true}, () => {
+            setTimeout(() => this.setState({copied: false}), 1500);
+        });
+    }
+
     render() {
+        const {copied} = this.state;
         const {background, name} = this.props;
         return (
-            <div className="color-box" style={{background}} >
-                <div className="copy-container">
-                    <div className="box-content">
-                        <label>{name}</label>
+            <CopyToClipboard text={background} onCopy={this.handleCopy}>
+                <div className="color-box" style={{background}} >
+                    <div style={{background}} className={`overlay ${copied && 'show'}`} />
+                    <div className="copy-container">
+                        <div className="box-content">
+                            <label>{name}</label>
+                        </div>
+                        <button className='copy-btn'>COPY</button>
                     </div>
-                    <button className='copy-btn'>COPY</button>
+                    <label className="see-more">MORE</label>
                 </div>
-                <label className="see-more">MORE</label>
-            </div>
+            </CopyToClipboard>
         );
     }
 }
