5244cb53592ec5660d5e0a1968a34b5d85c4b59f
Ratik Vig <ratikvig@gmail.com>
2020-09-20 01:16:40 +0530

    animation updates

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -22,7 +22,11 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={background} onCopy={this.handleCopy}>
                 <div className="color-box" style={{background}} >
-                    <div style={{background}} className={`overlay ${copied && 'show'}`} />
+                    <div style={{background}} className={`overlay ${copied ? 'show':'hide'}`} />
+                    <div className={`color-badge ${copied ? 'show': 'hide'}`}>
+                        <h1>COPIED!</h1>
+                        <label>{background}</label>
+                    </div>
                     <div className="copy-container">
                         <div className="box-content">
                             <label>{name}</label>
