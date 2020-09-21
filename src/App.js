1151269273fe82bb710cc81565828487dde45389
Ratik Vig <ratikvig@gmail.com>
2020-09-22 02:28:52 +0530

    added level slider

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import generatePalette from "./utils/shadeGenerator";
 
 class App extends React.Component{
   render(){
-      console.log(generatePalette(seedColors[4]))
     return(
         <div className="App">
-            <Palette {...seedColors[1]} />
+            <Palette palette={generatePalette(seedColors[1])} />
         </div>
     );
   }
--- src/components/Palette.js
+++ src/components/Palette.js
@@ -3,15 +3,31 @@ import ColorBox from "./ColorBox";
 import '../css/Palette.css';
 
 class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            level: 500
+        }
+    }
+
+    handleChange = event => {
+        this.setState({level: event.target.value})
+    }
 
     render() {
-        const {colors} = this.props;
+        const {colors} = this.props.palette;
+        const {level} = this.state;
         return (
             <div className="palette">
                 {/*Navbar*/}
+                <div style={{display: 'flex', alignItems: 'centerg'}}>
+                    <label>{level}</label>
+                    <input type="range" min="100" max="900" step="100" value={level} onChange={this.handleChange}/>
+                </div>
+
                 <div className="palette-colors">
-                    {colors.map(color => (
-                        <ColorBox background = {color.color} name = {color.name} />
+                    {colors[level].map(color => (
+                        <ColorBox background={color.hex} name={color.name}/>
                     ))}
                 </div>
                 {/*footer*/}
